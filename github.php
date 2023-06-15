ad7604c766a12371347f672c6cd1d3a0483a5dc8
Srikanth <srikanth8605@gmail.com>
2023-06-15 12:23:05 +0530

    learning github

---
+++ github.php
@@ -0,0 +1,33 @@
+Sure! Here are some commonly used Git commands that you will frequently encounter when working with GitHub:
+
+git init: Initializes a new Git repository in your current directory.
+
+git clone <repository URL>: Creates a local copy of a remote repository on your machine.
+
+git add <file>: Adds a file to the staging area, preparing it for a commit.
+
+git commit -m "commit message": Creates a new commit with the changes in the staging area and adds a descriptive commit message.
+
+git push: Pushes your local commits to a remote repository.
+
+git pull: Fetches and merges changes from a remote repository into your local branch.
+
+git branch: Lists all branches in your repository.
+
+git branch <branch name>: Creates a new branch.
+
+git checkout <branch name>: Switches to a specified branch.
+
+git merge <branch name>: Merges a branch into the current branch.
+
+git status: Shows the current state of your repository, including the files that are modified, added, or deleted.
+
+git log: Displays a log of all commits in the repository, showing the commit messages, authors, dates, and commit hashes.
+
+git remote: Lists the remote repositories associated with your local repository.
+
+git remote add <name> <repository URL>: Adds a remote repository to your local repository.
+
+git remote remove <name>: Removes a remote repository from your local repository.
+
+These are just a few of the many Git commands available. It's important to familiarize yourself with these commands and explore the Git documentation for more advanced usage and options.
